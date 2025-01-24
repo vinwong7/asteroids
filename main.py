854843df31245c0bc2e910e6176d848b0a08f8f3
lyroth <earthmage11@gmail.com>
2025-01-23 19:51:24 -0500

    B: Added constants file, updated .gitignore, main now creates screen

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black") #Fill the screen black
+        pygame.display.flip() #Refresh screen
 
 if __name__ == "__main__":
     main()
