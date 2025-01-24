eab40c79d2fa0daef531bb2e0ce78f886545c87f
lyroth <earthmage11@gmail.com>
2025-01-23 20:38:12 -0500

    C: created circleshape and player model, updated main to create player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,13 +9,22 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    p1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS) #Create player model
+
+    #Game Loop
     while True:
+        #Quit line
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black") #Fill the screen black
+        p1.draw(screen) #Draw player model
         pygame.display.flip() #Refresh screen
+
+        dt = clock.tick(60)/1000 #.tick keeps game at 60 fps
 
 if __name__ == "__main__":
     main()
