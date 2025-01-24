a68280ef2f87dc41bd1dfc597a2fae34f00c51b2
lyroth <earthmage11@gmail.com>
2025-01-23 22:57:49 -0500

    D: updated player to move and rotate; update main to incorporate player and incorporate groups

--- main.py
+++ main.py
@@ -8,23 +8,44 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #Creating game objects
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
-    p1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS) #Create player model
+    #Create containers for items to go into, based on what actions they are taking
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
+    #Player will be drawn and updated
+    Player.containers = (drawable, updatable)
+
+    #Create player model
+    p1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS) 
+    
     #Game Loop
     while True:
         #Quit line
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="black") #Fill the screen black
-        p1.draw(screen) #Draw player model
-        pygame.display.flip() #Refresh screen
 
-        dt = clock.tick(60)/1000 #.tick keeps game at 60 fps
+        #Fill the screen black    
+        screen.fill(color="black") 
+        
+        #Loop through drawable group and draw the objects
+        for i in drawable:
+            i.draw(screen)
+
+        #Loop through updatable group and update the objects
+        for i in updatable:
+            i.update(dt)
+        
+        #Refresh screen
+        pygame.display.flip() 
+
+        #.tick keeps game at 60 fps, dt variable keeps track of time change
+        dt = clock.tick(60)/1000 
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -5,7 +5,8 @@ class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-    # in the player class
+    
+    #Player model is based on triangle, though hitbox is circle
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -16,4 +17,22 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen,"white",self.triangle(),2)
-        
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt 
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
